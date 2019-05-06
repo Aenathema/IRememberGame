79886c6b253eaa1b593449384cc9aaf3b596f97e
Rhianna Jones <rhianna-j@hotmail.com>
2019-05-06 15:51:30 +0800

    Starting Level 1

---
+++ I_REMEMBER/Assets/CameraFollowScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+	public GameObject objectToFollow;
+    
+    public float speed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   float interpolation = speed * Time.deltaTime;
+        
+        Vector3 position = this.transform.position;
+        position.x = Mathf.Lerp(this.transform.position.x, objectToFollow.transform.position.x, interpolation);
+        
+        this.transform.position = position;   
+    }
+}
