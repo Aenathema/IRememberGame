42f3bbd02a847e6921faf226e613160d6886b4bc
Rhianna Jones <rhianna-j@hotmail.com>
2019-05-15 23:11:09 +0800

    Attempting to code pickup

---
+++ I_REMEMBER/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private int count;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	 void OnTriggerEnter(Collider other) 
+    {
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+		other.gameObject.SetActive (false);
+		count = count + 1;
+		}
+	}
+	}
+}
