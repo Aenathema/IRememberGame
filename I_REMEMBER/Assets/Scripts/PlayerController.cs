df079667ae39dfb7bf77a6f1a31132e56a21e948
SharSharSharShar <49180478+SharSharSharShar@users.noreply.github.com>
2019-05-30 15:24:04 +0800

    Dialogue pop-ups
    Managed to do some dialogue pop-ups. Have the basis down for the item pickup, still trying to make it work :+1:

--- I_REMEMBER/Assets/Scripts/PlayerController.cs
+++ I_REMEMBER/Assets/Scripts/PlayerController.cs
@@ -1,27 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
 
 public class PlayerController : MonoBehaviour
 {
 	private int count;
+    public float speed;
+    public Text countText;
+    public Text winText;
 	
     // Start is called before the first frame update
     void Start()
     {
         count = 0;
+        SetCountText ();
+        winText.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-	 void OnTriggerEnter(Collider other) 
+    }
+	   void OnTriggerEnter(Collider other) 
     {
-		if (other.gameObject.CompareTag ("Pick Up"))
-		{
-		other.gameObject.SetActive (false);
-		count = count + 1;
-		}
-	}
+        if (other.gameObject.CompareTag ( "Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Memories: " + count.ToString ();
+        if (count >= 1)
+        {
+            winText.text = "You collected all of the items!";
+        }
 	}
 }
